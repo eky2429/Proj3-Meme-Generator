bea7cf1c16062ed49b0ef568455cc41e4dd17ced
eky2429 <eky2429@gmail.com>
2024-12-16 17:16:31 -0600

    Included form in site and added styling to form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import Header from './components/Header.jsx'
+import Meme from './components/Meme.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Header />
+    <>
+      <Header />
+      <Meme />
+    </>
   )
 }
 
